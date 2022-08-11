9c1237a833ad2c8b7b8b178a069ccc0d530b4b16
AugustoMagalhaes <augustommg@gmail.com>
2022-08-11 05:35:03 -0300

    feat: implements Map with customTsConfig options.

---
+++ tsconfigOptions.js
@@ -0,0 +1,10 @@
+const customTsconfig = new Map([
+  ['--target', ['ES2015', 'es2018', 'es2019', 'es2020', 'es2021', 'es2022', 'esnext']],
+  ['--module', ['commonjs', 'es6', 'es2020', 'es2021', 'esnext', 'node16', 'nodenext']],
+  ['--allowJs', ['true', 'false']],
+  ['--strict', ['true', 'false']],
+  ['--esModuleInterop', ['true', 'false']],
+  ['--skipLibCheck', ['true', 'false']],
+  ['--forceConsistentCasingInFileNames', ['true', 'false']],
+  ['--preserveConstEnums', ['true', 'false']],
+]);
