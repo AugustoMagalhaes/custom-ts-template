8190bb21e4d0ed34d0459e73b7031c20db7042a1
AugustoMagalhaes <augustommg@gmail.com>
2022-08-14 07:53:21 -0300

    feat: adds askRecommendedOrCustomOptions() call to index.js.

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 'use strict';
 
 import { mainInstaller } from './src/installer.js';
+import askRecommendedOrCustomOptions from './src/tsconfigOptions.js';
 
 // check if file is main module to avoid autorun when importing index.js
 if (import.meta.url === `file://${process.argv[1]}`) {
   await mainInstaller();
+
+  await askRecommendedOrCustomOptions();
 }
