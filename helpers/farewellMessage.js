c2a62480fd1afcc51c4e9734fd1e275135e046ff
AugustoMagalhaes <augustommg@gmail.com>
2022-08-14 08:50:45 -0300

    feat: implements farewellMessage to display at the end.

---
+++ helpers/farewellMessage.js
@@ -0,0 +1,24 @@
+import chalk from 'chalk';
+import figlet from 'figlet';
+import gradient from 'gradient-string';
+import sleep from './sleep.js';
+
+export async function farewellMessage() {
+  await sleep();
+  const msg = 'Happy Coding!';
+  const figletConfig = figlet.textSync(msg, {
+    font: 'Standard',
+    horizontalLayout: 'default',
+    verticalLayout: 'fitted',
+    width: 120,
+  });
+  console.log(gradient.pastel('-'.repeat(process.stdout.columns)));
+  console.log(gradient.pastel(figletConfig));
+  console.log(gradient.pastel('-'.repeat(process.stdout.columns)), '\n');
+  console.log(
+    chalk.bold.green(
+      'This script was made by AugustoMagalhaes (https://github.com/AugustoMagalhaes)',
+    ),
+  );
+  console.log(chalk.bold.white.bgGreen('Any suggestions and comments are appreciated :) \n'));
+}
