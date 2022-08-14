6579992001d76e7c49e938ce524ad5eea6809eb9
AugustoMagalhaes <augustommg@gmail.com>
2022-08-14 08:50:59 -0300

    chore: adds farewellMessage to index.js.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
+import { farewellMessage } from './helpers/farewellMessage.js';
 import { mainInstaller } from './src/installer.js';
 import askRecommendedOrCustomOptions from './src/tsconfigOptions.js';
 
@@ -9,4 +10,6 @@ if (import.meta.url === `file://${process.argv[1]}`) {
   await mainInstaller();
 
   await askRecommendedOrCustomOptions();
+
+  await farewellMessage();
 }
