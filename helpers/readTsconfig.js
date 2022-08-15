2aede0107079203aa6788cf3c05acfa7dcb12508
AugustoMagalhaes <augustommg@gmail.com>
2022-08-15 11:44:47 -0300

    chore: creates separate function customRead() and exports it.

--- helpers/readTsconfig.js
+++ helpers/readTsconfig.js
@@ -5,9 +5,14 @@ function stripComments(data) {
   return data.replace(/\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g, (m, g) => (g ? '' : m));
   // thanks https://stackoverflow.com/questions/33483667/how-to-strip-json-comments-in-javascript
 }
+
+export async function customRead(path) {
+  const data = await fs.readFile(path, { encoding: 'utf8' });
+  return data;
+}
 export default async function readAndParse() {
   try {
-    let data = await fs.readFile('./tsconfig.json', { encoding: 'utf8' });
+    let data = await read('./tsconfig.json');
     const commentlessData = stripComments(data);
 
     const parsedData = JSON.parse(commentlessData);
