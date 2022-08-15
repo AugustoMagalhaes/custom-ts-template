4a254850ce38bf10cdad52b0798c4e0e2f4c0090
AugustoMagalhaes <augustommg@gmail.com>
2022-08-15 12:37:10 -0300

    feat: implements writeAuxFiles to create gitignore file after all operations.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 'use strict';
 
 import { farewellMessage } from './helpers/farewellMessage.js';
+import gitignoreContent from './helpers/fileContents/gitignoreContent.js';
+import writeAuxFiles from './helpers/writeAuxiliaryFiles.js';
 import { mainInstaller } from './src/installer.js';
 import askRecommendedOrCustomOptions from './src/tsconfigOptions.js';
 
@@ -10,6 +12,8 @@ if (import.meta.url === `file://${process.argv[1]}`) {
   await mainInstaller();
 
   await askRecommendedOrCustomOptions();
+
+  await writeAuxFiles(gitignoreContent, './.gitignore', '.gitignore');
 
   await farewellMessage();
 }
